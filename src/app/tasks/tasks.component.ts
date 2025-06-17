016d2dbbd124150103ba1cde0ee409d118afd7d6
futsurdmitriy <futsurdmitriy@gmail.com>
2025-06-17 16:24:48 +0200

    Change remove task logic via service.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -22,10 +22,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(taskId: string) {
-    return this.tasksService.removeTask(taskId);
-  }
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
