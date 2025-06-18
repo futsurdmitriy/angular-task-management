ea6d9f6a207b2d620777156452f65c7a58f58a02
futsurdmitriy <futsurdmitriy@gmail.com>
2025-06-18 10:50:38 +0200

    Migrating All Components To Use Modules

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
+
 @Component({
     selector: 'app-header',
-    standalone: true,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
